a9865042d2dc1105301b3ef361acf50bee2f86eb
Robzlegz <robzlegz@gmail.com>
2021-02-09 18:06:17 +0200

    installing modules

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./Search.css";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 const Search = () => {
     return (
