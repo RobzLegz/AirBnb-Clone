978c8e10db0cd04e8745f1ab70ffbc02a9d08a61
Robzlegz <robzlegz@gmail.com>
2021-02-10 11:58:44 +0200

    styling changes

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Card.css";
 
-const Card = ({source, title, description, price}) => {
+const Card = ({source, title, description, price, aboutTitle}) => {
     return (
         <div className={ description ? "card" : "biggerCard"}>
             <img src={source} alt={title} />
             <div className="cardInfo">
-                <h2>{title}</h2>
+                {aboutTitle && <h2 className="aboutTitle">{aboutTitle}</h2>}
+                {title && <h2>{title}</h2>}
                 <h4>{description}</h4>
                 {price && <h3>{price}€/night</h3>}
             </div>
